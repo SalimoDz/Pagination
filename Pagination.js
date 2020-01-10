9b98398b1116cc1f142dfc501a32b3fbf1c62003
SalimoDz <37343186+SalimoDz@users.noreply.github.com>
2020-01-10 17:28:18 +0100

    Create Pagination.js

---
+++ Pagination.js
@@ -0,0 +1,9 @@
+This is the html code part to initialize
+
+<ul class="pagination row d-flex justify-content-center">
+				<li class="page-item previousp"><a class="page-link" href="#"><</a></li>
+				<li id="firstp" class="page-item active"><a class="page-link" href="<?php echo base_url("Tuto/FileSharing"); ?>">1</a></li>
+
+				<li id="lastp" class="page-item"><a class="page-link lastpage" href="<?php echo base_url("Tuto/LastPage"); ?>">Last</a></li>
+				<li  class="page-item nextp"><a class="page-link" href="#">></a></li>
+</ul>
